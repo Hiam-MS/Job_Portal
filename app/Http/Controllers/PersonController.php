f4f4de26d919521bf28718407ced21844f49d6f6
Hiam-MS <hiam.ms98@gmail.com>
2021-09-01 10:28:19 +0300

    pe

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -32,7 +32,6 @@ class PersonController extends Controller
         
         $Person = Person::find($id);
         return view('test',compact('Person'));
-<<<<<<< HEAD
   
     }
 
@@ -61,40 +60,6 @@ class PersonController extends Controller
     public function res_det()
     {
         return view('person.ResuemDetail');
-=======
-        
-    }
-
-
-    public function createResume()
-    {
-        return view('person.addResume');
-    }
-
-
-    public function store(Request $Request)
-
-    {
-        $person =new Person ;
-            $person->name = $Request->input("name");
-            $person->email =  $Request->input("email");
-            $person->gender= $Request->input("gender");
-            $person->dob= $Request->input("dob");
-            $person->place_Of_b = $Request->input("place_Of_b");
-            $person->national_number= $Request->input("national_number");
-            $person->marital_status= $Request->input("marital_status");
-            $person->military_service= $Request->input("military_service");
-            $person->Current_address= $Request->input("Current_address");
-            $person->fixed_phone= $Request->input("fixed_phone");
-            $person->mobile_number= $Request->input("mobile_number");
-            $person->img= $Request->input("img");
-            ///$person->lang= $Request->input("lang");
-
-        $person->save();
-        return redirect('/');
-       
-        
->>>>>>> 5fa4076c540ef10da2081696ca086cb1a06bc743
     }
 
 }
